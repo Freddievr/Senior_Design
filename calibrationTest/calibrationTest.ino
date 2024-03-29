374898c60d3bfe021328d25134183a64e2958d06
senpaiinduhsheet <83674373+senpaiinduhsheet@users.noreply.github.com>
2024-03-29 15:40:41 -0400

    Update calibrationTest.ino
    Added some calibration functions still under testing phase

--- calibrationTest/calibrationTest.ino
+++ calibrationTest/calibrationTest.ino
@@ -1,67 +1,55 @@
+// Calibration button pins
+#define buttonPin1 6
+#define buttonPin2 5
+// Stepper pins
+#define dirPinM1 8
+#define stepPinM1 9
+// Motor positions [initial final]
+int steps[2];
+// Motor movement variables
 int x;
-int motorDistance = 8000;
+int motorDistance = 3000;
 int motorSpeed = 300;
-const int buttonPin1 = 6;  // the number of the pushbutton pin
-const int buttonPin2 = 5;
-int buttonState;
 
 void setup() {
-  pinMode(9, OUTPUT);  // set Pin9 as PUL
-  pinMode(8, OUTPUT);  // set Pin8 as DIR
-    // initialize the pushbutton pin as an input:
+  pinMode(stepPinM1, OUTPUT);  // set Pin9 as PUL
+  pinMode(dirPinM1, OUTPUT);   // set Pin8 as DIR
+  // initialize the pushbutton pin as an input:
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
 }
 void loop() {
-  while(buttonPress1()){
-    digitalWrite(8, HIGH);     // set high level direction
-    for (x = 0; x < motorDistance; x++)  // repeat 400 times a revolution when setting 400 on driver
-    {
-      digitalWrite(9, HIGH);   // Output high
-      delayMicroseconds(motorSpeed);  // set rotate speed
-      digitalWrite(9, LOW);    // Output low
-      delayMicroseconds(motorSpeed);  // set rotate speed
-    }
+  while (digitalRead(buttonPin1)) {
+    digitalWrite(dirPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
   }
-
-  while(buttonPress2()){
-    digitalWrite(8, LOW);     // set high level direction
-    for (x = 0; x < motorDistance; x++)  // repeat 400 times a revolution when setting 400 on driver
-    {
-      digitalWrite(9, HIGH);   // Output high
-      delayMicroseconds(motorSpeed);  // set rotate speed
-      digitalWrite(9, LOW);    // Output low
-      delayMicroseconds(motorSpeed);  // set rotate speed
-    }
+// Slowly move away from switch until switch is open
+  while (!digitalRead(buttonPin1)) {
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
   }
-}
-
-bool buttonPress1() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin1);
-
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // return true for button press
-    return true;
-  } 
-  else {
-    // reset to false
-    return false;
+  steps[0] = 0;
+  delay(10);
+// Moves in opposite direction to find max travel distance
+  while (digitalRead(buttonPin2)) {
+    digitalWrite(dirPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+  }
+// Slowly move away from switch until switch is open
+  while (!digitalRead(buttonPin2)) {
+    digitalWrite(stepPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
   }
 }
 
-bool buttonPress2() {
-  // read the state of the pushbutton value:
-  buttonState = digitalRead(buttonPin2);
+void calibrateHomeMinMax(){
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // return true for button press
-    return true;
-  } 
-  else {
-    // reset to false
-    return false;
-  }
 }
