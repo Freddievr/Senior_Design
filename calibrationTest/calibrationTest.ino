a2287ad7796ae098bab47b449757f2e464a4a28c
senpaiinduhsheet <83674373+senpaiinduhsheet@users.noreply.github.com>
2024-04-01 17:22:25 -0400

    Update calibrationTest.ino
    Button configuration works properly and now counts where the stepper position is. Don't know if this is in relation to rotations or not.

--- calibrationTest/calibrationTest.ino
+++ calibrationTest/calibrationTest.ino
@@ -5,13 +5,14 @@
 #define dirPinM1 8
 #define stepPinM1 9
 // Motor positions [initial final]
-int steps[2];
+int steps[2] = {0, 0};
 // Motor movement variables
 int x;
 int motorDistance = 3000;
-int motorSpeed = 300;
+int motorSpeed = 200; // Lower is faster
 
 void setup() {
+  Serial.begin(9600);
   pinMode(stepPinM1, OUTPUT);  // set Pin9 as PUL
   pinMode(dirPinM1, OUTPUT);   // set Pin8 as DIR
   // initialize the pushbutton pin as an input:
@@ -20,37 +21,42 @@ void setup() {
 }
 void loop() {
   
-//   while (!digitalRead(buttonPin1)) {
-//     digitalWrite(dirPinM1, HIGH);
-//     digitalWrite(stepPinM1, HIGH);
-//     delayMicroseconds(motorSpeed);
-//     digitalWrite(stepPinM1, LOW);
-//     delayMicroseconds(motorSpeed);
-//   }
-// // Slowly move away from switch until switch is open
-//   while (digitalRead(buttonPin1)) {
+  while (!digitalRead(buttonPin1)) {
+    digitalWrite(dirPinM1, HIGH);
+    digitalWrite(stepPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+  }
+// Slowly move away from switch until switch is open
+  while (digitalRead(buttonPin1)) {
     digitalWrite(dirPinM1, LOW);
-//     digitalWrite(stepPinM1, HIGH);
-//     delayMicroseconds(motorSpeed);
-//     digitalWrite(stepPinM1, LOW);
-//     delayMicroseconds(motorSpeed);
-//   }
-//   steps[0] = 0;
-//   delay(10);
-// // Moves in opposite direction to find max travel distance
-//   while (digitalRead(buttonPin2)) {
-//     digitalWrite(dirPinM1, LOW);
-//     delayMicroseconds(motorSpeed);
-//     digitalWrite(stepPinM1, LOW);
-//     delayMicroseconds(motorSpeed);
-//   }
-// // Slowly move away from switch until switch is open
-//   while (!digitalRead(buttonPin2)) {
-//     digitalWrite(stepPinM1, HIGH);
-//     delayMicroseconds(motorSpeed);
-//     digitalWrite(stepPinM1, LOW);
-//     delayMicroseconds(motorSpeed);
-//   }
+    digitalWrite(stepPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+  }
+  steps[0] = 0;
+  Serial.println(steps[0]);
+  delay(1000);
+// Moves in opposite direction to find max travel distance
+  while (!digitalRead(buttonPin2)) {
+    digitalWrite(dirPinM1, LOW);
+    digitalWrite(stepPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+    steps[1]++;
+  }
+// Slowly move away from switch until switch is open
+  while (digitalRead(buttonPin2)) {
+    Serial.println(steps[1]);
+    digitalWrite(dirPinM1, HIGH);
+    digitalWrite(stepPinM1, HIGH);
+    delayMicroseconds(motorSpeed);
+    digitalWrite(stepPinM1, LOW);
+    delayMicroseconds(motorSpeed);
+  }
   while(1){
     digitalWrite(stepPinM1, HIGH);
   }
