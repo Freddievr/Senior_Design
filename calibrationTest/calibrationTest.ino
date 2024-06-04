615886ef1ee8a56e58bc1d6de38d4bd57719bdfa
senpaiinduhsheet <83674373+senpaiinduhsheet@users.noreply.github.com>
2024-06-04 19:18:28 -0400

    Added variables

--- calibrationTest/calibrationTest.ino
+++ calibrationTest/calibrationTest.ino
@@ -9,7 +9,7 @@ int steps[2] = {0, 0};
 // Motor movement variables
 int x;
 int motorDistance = 3000;
-int motorSpeed = 100; // Lower is faster
+int motorSpeed = 400; // Lower is faster
 
 void setup() {
   Serial.begin(9600);
@@ -20,9 +20,8 @@ void setup() {
   pinMode(buttonPin2, INPUT_PULLUP);
 }
 void loop() {
-  
-  while (digitalRead(buttonPin1)) {
-    digitalWrite(dirPinM1, HIGH);
+  digitalWrite(dirPinM1, LOW);
+  while (!digitalRead(buttonPin1)) {
     digitalWrite(stepPinM1, HIGH);
     delayMicroseconds(motorSpeed);
     digitalWrite(stepPinM1, LOW);
@@ -30,8 +29,8 @@ void loop() {
   }
   delay(1000);
 // Slowly move away from switch until switch is open
-  while (!digitalRead(buttonPin1)) {
-    digitalWrite(dirPinM1, LOW);
+  digitalWrite(dirPinM1, HIGH);
+  while (digitalRead(buttonPin1)) {
     digitalWrite(stepPinM1, HIGH);
     delayMicroseconds(motorSpeed);
     digitalWrite(stepPinM1, LOW);
@@ -39,25 +38,26 @@ void loop() {
   }
   steps[0] = 0;
   Serial.println(steps[0]);
-  delay(1000);
+  // delay(1000);
 // Moves in opposite direction to find max travel distance
-  while (digitalRead(buttonPin2)) {
-    digitalWrite(dirPinM1, LOW);
+  digitalWrite(dirPinM1, HIGH);
+  while (!digitalRead(buttonPin2)) {
     digitalWrite(stepPinM1, HIGH);
     delayMicroseconds(motorSpeed);
     digitalWrite(stepPinM1, LOW);
     delayMicroseconds(motorSpeed);
     steps[1]++;
   }
+  delay(1000);
 // Slowly move away from switch until switch is open
-  while (!digitalRead(buttonPin2)) {
-    Serial.println(steps[1]);
-    digitalWrite(dirPinM1, HIGH);
+  digitalWrite(dirPinM1, LOW);
+  while (digitalRead(buttonPin2)) {
     digitalWrite(stepPinM1, HIGH);
     delayMicroseconds(motorSpeed);
     digitalWrite(stepPinM1, LOW);
     delayMicroseconds(motorSpeed);
   }
+  Serial.println(steps[1]);
   while(1){
     digitalWrite(stepPinM1, HIGH);
   }
--- simpleMotorTest/simpleMotorTest.ino
+++ simpleMotorTest/simpleMotorTest.ino
@@ -1,24 +1,26 @@
 int x;
+int motorDistance = 4000;
+int motorSpeed = 300;
 void setup() {
   pinMode(9, OUTPUT);  // set Pin9 as PUL
   pinMode(8, OUTPUT);  // set Pin8 as DIR
 }
 void loop() {
   digitalWrite(8, HIGH);     // set high level direction
-  for (x = 0; x < 400; x++)  // repeat 400 times a revolution when setting 400 on driver
+  for (x = 0; x < motorDistance; x++)  // repeat 400 times a revolution when setting 400 on driver
   {
     digitalWrite(9, HIGH);   // Output high
-    delayMicroseconds(500);  // set rotate speed
+    delayMicroseconds(motorSpeed);  // set rotate speed
     digitalWrite(9, LOW);    // Output low
-    delayMicroseconds(500);  // set rotate speed
+    delayMicroseconds(motorSpeed);  // set rotate speed
   }
   delay(1000);           //pause 1 second
   digitalWrite(8, LOW);  // set high level direction
-  for (x = 0; x < 400; x++) {
+  for (x = 0; x < motorDistance; x++) {
     digitalWrite(9, HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(motorSpeed);
     digitalWrite(9, LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(motorSpeed);
   }
   delay(1000);
 }
