d488911e26df64c99d7cda7806a91ecd12c93af1
Freddie Vasquez-Rios <161064448+Freddievr@users.noreply.github.com>
2024-06-18 16:50:09 -0700

    Add files via upload

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ import os
 from tkinter import filedialog
 from PIL import Image
 import serial.tools.list_ports
-
+import time
 
 customtkinter.set_appearance_mode(
     "Light")  # Modes: "System" (standard), "Dark", "Light"
@@ -20,11 +20,10 @@ class App(customtkinter.CTk):
 
     # configure window
     self.title("UNCC_ECE_PROBE")
-    self.geometry(f"{1400}x{750}")
+    self.geometry(f"{1150}x{750}")
 
     # configure grid layout (4x4)
-    self.grid_columnconfigure(1, weight=1)
-    self.grid_columnconfigure((2, 3), weight=1)
+    self.grid_columnconfigure((0,1,2), weight=1)
     self.grid_rowconfigure((0, 1, 2), weight=1)
 
     # create sidebar frame with widgets
@@ -66,14 +65,7 @@ class App(customtkinter.CTk):
       self.sidebar_frame, text="Arduino Path:", anchor="w")
     self.label_open_arduino.grid(row=7, column=0, padx=20, pady=(10, 0))
 
-    self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
-        self.sidebar_frame,
-        values=["Light", "Dark", "System"],
-        command=self.change_appearance_mode_event)
-    self.appearance_mode_optionemenu.grid(row=8,
-                                          column=0,
-                                          padx=20,
-                                          pady=(10, 10))
+  
     self.scaling_label = customtkinter.CTkLabel(self.sidebar_frame,
                                                 text="UI Scaling:",
                                                 anchor="w")
@@ -84,85 +76,32 @@ class App(customtkinter.CTk):
         command=self.change_scaling_event)
     self.scaling_optionemenu.grid(row=10, column=0, padx=20, pady=(10, 20))
 
-
-    # Create Picture
-    self.image1 = customtkinter.CTkImage(light_image=Image.open('Images/1.png'),
-    dark_image=Image.open('Images/1.png'),
-    size=(600,300))
-    self.image1_label = customtkinter.CTkLabel(self, text = "", image=self.image1)
-    self.image1_label.grid(column=1,row=0)
-  
-    # create textbox
-    #self.textbox = customtkinter.CTkTextbox(self, width=250)
-    #self.textbox.grid(row=0,
-    #                  column=1,
-    #                  padx=(20, 20),
-    #                  pady=(20, 20),
-    #                  sticky="nsew")
-
-    # create tabview Method Selection
+      # configure tab view and locate in grid
     self.tabview = customtkinter.CTkTabview(self, width=150)
     self.tabview.grid(row=0,
-                      column=3,
+                      column=1,
                       padx=(30, 30),
                       pady=(10, 10),
                       sticky="nsew")
-    self.tabview.add("Method 1")
-    self.tabview.add("Method 2")
-    self.tabview.tab("Method 1").grid_columnconfigure(
-        0, weight=1)  # configure grid of individual tabs
-    self.tabview.tab("Method 2").grid_columnconfigure(0, weight=1)
+    # add individual tabs
+    self.tabview.add("Parameters")
+    # configure grid of individual tabs
+    self.tabview.tab("Parameters").grid_columnconfigure(
+        0, weight=1)  
 
+    self.optionmenu_1_var = customtkinter.StringVar(value="Select Variable")
     self.optionmenu_1 = customtkinter.CTkOptionMenu(
-        self.tabview.tab("Method 1"),
+        self.tabview.tab("Parameters"),
         dynamic_resizing=False,
-        values=["Gap Average", "# of Fingers"])
-    self.optionmenu_1.grid(row=0, column=0, padx=20, pady=(20, 10))
-  
-    self.string_input_button = customtkinter.CTkButton(
-        self.tabview.tab("Method 1"),
-        text="Edit Variable",
-        command=self.open_input_dialog_event)
-    self.string_input_button.grid(row=1, column=0, padx=20, pady=(10, 10))
-
-    self.optionmenu_2= customtkinter.CTkOptionMenu(
-      self.tabview.tab("Method 2"),
-      dynamic_resizing=False,
-      values=["Gap Average", "Gridline Average", "Strip Width"])
-    self.optionmenu_2.grid(row=0, column=0, padx=20, pady=(20, 10))
-
-    self.string_input_button = customtkinter.CTkButton(
-      self.tabview.tab("Method 2"),
-      text="Edit Variable",
-      command=self.open_input_dialog_event)
-    self.string_input_button.grid(row=1, column=0, padx=20, pady=(10, 10))
-
-    # create radiobutton frame
-    self.radiobutton_frame = customtkinter.CTkFrame(self)
-    self.radiobutton_frame.grid(row=0,
-                                column=2,
-                                padx=(20, 10),
-                                pady=(20, 10),
-                                sticky="n")
-    self.radio_var = tkinter.IntVar(value=0)
-    self.label_radio_group = customtkinter.CTkLabel(
-        master=self.radiobutton_frame, text="Testing Method:", width = 100)
-    self.label_radio_group.grid(row=0,
-                                column=2,
-                                padx=20,
-                                pady=0,
-                                sticky="n")
-    self.radio_button_1 = customtkinter.CTkRadioButton(
-        master=self.radiobutton_frame, variable=self.radio_var, value=0, text="Method 1")
-    self.radio_button_1.grid(row=1, column=2, pady=10, padx=20, sticky="n")
-    self.radio_button_2 = customtkinter.CTkRadioButton(
-        master=self.radiobutton_frame, variable=self.radio_var, value=1, text="Method 2")
-    self.radio_button_2.grid(row=2, column=2, pady=10, padx=20, sticky="n")
-  
+        values=["Gap Width", "# of Fingers"],
+        command=open_input_parameters,
+        variable=self.optionmenu_1_var)
+    self.optionmenu_1.grid(row=0, column=0, padx=10, pady=(10, 10))
+        
     # create progressbar frame
     self.slider_progressbar_frame = customtkinter.CTkFrame(
         self, fg_color="transparent")
-    self.slider_progressbar_frame.grid(row=2,
+    self.slider_progressbar_frame.grid(row=6,
                                        column=1,
                                        padx=(20, 0),
                                        pady=(20, 0),
@@ -177,16 +116,10 @@ class App(customtkinter.CTk):
                             pady=(10, 10),
                             sticky="w")
 
-    # create Combobox (Method 1/2)
-    #self.method_var = tkinter.IntVar(value=0)
-    #self.combobox = customtkinter.CTkComboBox(master=self.method_frame, values=["Method 1", "Method 2"],
-    #                                 command=self.combobox_callback, variable=self.method_var)
-    #self.method_var.set("Method 1")
-
   # Start Button
     self.button_frame = customtkinter.CTkFrame(self)
     self.button_frame.grid(row=2,
-                              column=3,
+                              column=2,
                               padx=(20, 20),
                               pady=(10, 10),
                               sticky="n")
@@ -200,17 +133,12 @@ class App(customtkinter.CTk):
     self.stop_button = customtkinter.CTkButton(
       master=self.button_frame, command=self.button_stop, fg_color="red", text="Stop", width = 150)
     self.stop_button.grid(row=3, column=2, pady=20, padx=20, sticky="n")
-
   # set default values
     self.stop_button.configure(state="disabled")
     self.resume_button.configure(state="disabled")
-    self.appearance_mode_optionemenu.set("Light")
     self.scaling_optionemenu.set("100%")
     self.progressbar_1.configure(mode="determinate",determinate_speed= 0.1)
-    #self.textbox.insert(
-    #  0.0, "Demo for UNCC_ECE_Probe.\n\n " +
-    #  "Blank Window \n \n "
-    #  * 4)
+
 # FUNCTIONS Define
 
   def button_stop(self):    
@@ -221,14 +149,14 @@ class App(customtkinter.CTk):
     self.start_button.configure(state="enabled", text = "Start")
     self.stop_button.configure(state="disabled")
 
-
   def button_start(self):
     print("Program Started")
     self.progressbar_1.start()
     self.resume_button.configure(state="enabled")
     self.start_button.configure(state="disabled",text_color_disabled = "green", text = "Start")
     self.stop_button.configure(state="enabled")
-    self.connect_arduino()
+    self.send_button = customtkinter.CTkButton(self.sidebar_frame, text= "Send Values", command=self.var_send_button)
+    self.send_button.grid(row=5, column=0, padx=20, pady=10)
 
   def button_resume(self):
     print("button clicked")
@@ -243,12 +171,12 @@ class App(customtkinter.CTk):
     os.system('"%s' % file_path)
 
   def open_IV_program(self):
-    IV_path = '"c:/Users/Freddie Vasquez-Rios/Downloads/test.txt"'
-    self.label_open_iv.configure(text=IV_path)
+    IV_path = '"C:/Users/PVRL-01/Documents/S24PROBE/IV.exe"'
+    self.label_open_iv.configure(text=IV_path) 
     os.system('"%s' % IV_path)
 
   def open_arduino_program(self):
-    arduino_path = filedialog.askopenfilename()
+    arduino_path = '"C:/Users/PVRL-01/Documents/S24PROBE/ArduinoIDE/ArduinoIDE.exe"'
     self.label_open_arduino.configure(text=arduino_path)
     os.system('"%s' % arduino_path)
     
@@ -256,28 +184,55 @@ class App(customtkinter.CTk):
     ports = serial.tools.list_ports.comports()
     serial_inst = serial.Serial()
     ports_list = []
-    port_val = "COM5" ## NEEDS TO BE FIXED FOR PORT_VAL NOT READING
+    port_val = 'COM3'
 
     for port in ports:
         ports_list.append(str(port))
         print(str(port))
-              
-    val = self.COM_input_dialog()
-    for x in range(0,len(ports_list)):
-        if ports_list[x].startswith("COM" + str(val)):
-            port_val = "COM" + str(val)
-            print(serial_inst.port)
+    
+    # Commented OUT FOR DEMO          #
+    #for x in range(0,len(ports_list)):
+    #    val = self.COM_input_dialog()
+    #    if ports_list[x].startswith("COM" +str(val)):
+    #        port_val = "COM" + str(val)
+    #        print(serial_inst.port)
 
     serial_inst.baudrate = 9600
     serial_inst.port = port_val
     serial_inst.open()
 
+    global send_button
+    #send_button = 0   #CHANGE
+    # Send Variable Values Button   #FIX
+    #self.send_button = customtkinter.CTkButton(self.sidebar_frame, text= "Send Values", command=self.var_send_button)
+    #self.send_button.grid(row=5, column=0, padx=20, pady=10)
+       
+        
+    ## SEND VARIABLES 
     while True:
-        command = input("Arduino Command: (ON/OFF): ")
-        serial_inst.write(command.encode('utf-8'))  #NEEDS TO BE UPDATED FOR GUI
-
-        if command == 'EXIT':
-            return None  
+        global gap_width
+        global num_fingers
+     
+        print("begin loop") 
+        #input("Press Enter to continue...")
+        #print("gap width", gap_width)
+        #print("# of fingers:", num_fingers)
+        #gap_width = gap_width + '\r'
+        #num_fingers = num_fingers + '\r'
+        #serial_inst.write(gap_width.encode('utf-8'))  
+        #serial_inst.write(num_fingers.encode('utf-8'))
+        
+        
+        if send_button == 1:
+          variables = gap_width + "," + num_fingers
+          variables = variables + '\n'
+          serial_inst.write(variables.encode('utf-8'))
+          #serial_inst.flush()
+          print("SENT:", variables) 
+          #time.sleep(5)
+          #return None       #RUNS IN INFINITE LOOP CANNOT RUN IF THIS IS UNCOMMENTED (NEEDS FIX ASAP) 
+        else:
+            return None 
 
   def UNO_input_dialog(self): 
     val = customtkinter.CTkInputDialog(text="Arduino Command: (ON/OFF): ",
@@ -289,26 +244,50 @@ class App(customtkinter.CTk):
                                           title="Select Port")
     print("Selected Port:", val.get_input())
     return val
+        
 # given
   def open_input_dialog_event(self):
     dialog = customtkinter.CTkInputDialog(text="Type in Value:",
                                           title="Input Variables")
     print("Input Value:", dialog.get_input())
 
-  def change_appearance_mode_event(self, new_appearance_mode: str):
+  def change_appearance_mode_event(self, new_appearance_mode: str):   #Remove near end
     customtkinter.set_appearance_mode(new_appearance_mode)
 
   def change_scaling_event(self, new_scaling: str):
     new_scaling_float = int(new_scaling.replace("%", "")) / 100
     customtkinter.set_widget_scaling(new_scaling_float)
+    
+  def var_send_button(self): # Send Values Button # NOT NEEDED (Placeholder)
+    global send_button
+    send_button = 1
+    self.connect_arduino()
+    return send_button
 
-  def sidebar_button_event(self):
-    print("sidebar_button click")
-
-  def combobox_callback(choice):
-    print("Selected Method:", choice)
-
+def open_input_parameters(selection):
+        dialog = customtkinter.CTkInputDialog(text="Type in Value:",
+                                          title=selection)
+        var_name = selection
+        var_data = dialog.get_input()
+               
+        if (var_name == "Gap Width"):
+          global gap_width
+          gap_width = var_data
+          #print("gap width:", gap_width)
+          label_gap_width = customtkinter.CTkLabel(app, text = "Gap Width: " + gap_width)   #MAKE TRANSPARENT AND INIT EARLIER, .UPDATE TEXT AND COLOR HERE
+          label_gap_width.grid(row=0, column=2, padx=2, pady=80, sticky="n")
+          return gap_width  
+        else:
+          global num_fingers
+          num_fingers = var_data
+          #print("# of fingers:", num_fingers)
+          label_num_fingers = customtkinter.CTkLabel(app, text = "# of Fingers: " + num_fingers)
+          label_num_fingers.grid(row=0, column=2, padx=2, pady=2, sticky="ew")
+          return num_fingers
+ 
 if __name__ == "__main__":
   app = App()
   app.mainloop()
-
+  
+  # PYTHON TO EXE line command below
+  # pyinstaller gui.py --onefile --windowed 
