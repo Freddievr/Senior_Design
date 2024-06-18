14cd992e2e4dd4214f4b0754866750f1126fe4c7
senpaiinduhsheet <83674373+senpaiinduhsheet@users.noreply.github.com>
2024-06-18 11:49:14 -0400

    Motor Testing
    Was working, but now forward direction does not work anymore.

--- calibrationTest/calibrationTest.ino
+++ calibrationTest/calibrationTest.ino
@@ -5,36 +5,102 @@
 #include <Stream.h>
 
 // Defined Variables
-#define MAX_MOTOR_SPEED 100
+#define MAX_MOTOR_SPEED 2000.0
+#define HORZ_HOME_SW 11
+#define VERT_HOME_SW 10
+#define CALIBRATE_MOTOR_SPEED 1000.0
 
 // Global Variables
 String streamData;
 String parsedData;
+bool homeComplete = false;
+long initialHoming = 400;
 
 // Initialize Stepper motors
 // Function nomenclature motorName(# of Required Input pin(STEP and DIR), STEP, DIR)
-AccelStepper stepper1(AccelStepper::FULL2WIRE, 5, 6);
-AccelStepper stepper2(AccelStepper::FULL2WIRE, 7, 8);
+AccelStepper stepper1(AccelStepper::FULL2WIRE, 7, 8);
+AccelStepper stepper2(AccelStepper::FULL2WIRE, 6, 5);
 
 // Creates a group for steppers motors to be added
-MultiStepper steppers;
+// MultiStepper steppers;
 
 // Forward declare functions
 String parseDataStream();
+void calibrateHome();
 
 void setup() {
   Serial.begin(9600);
 
-  // Configure each stepper 100 steps per second
+  // Configure each stepper # steps per second
   stepper1.setMaxSpeed(MAX_MOTOR_SPEED);
   stepper2.setMaxSpeed(MAX_MOTOR_SPEED);
 
   // Add them to MultiStepper group to manage
-  steppers.addStepper(stepper1);
-  steppers.addStepper(stepper2);
+  // steppers.addStepper(stepper1);
+  // steppers.addStepper(stepper2);
+
+  // Configure button settings
+  pinMode(HORZ_HOME_SW, INPUT_PULLUP);
+  pinMode(VERT_HOME_SW, INPUT_PULLUP);
+  stepper1.setCurrentPosition(0);
 }
 void loop() {
+  // calibrateHome();
+  // stepper1.setCurrentPosition(0);
+  stepper1.moveTo(initialHoming);
+  // stepper1.setMaxSpeed(CALIBRATE_MOTOR_SPEED);
+  stepper1.setSpeed(CALIBRATE_MOTOR_SPEED);
+  // stepper1.setAcceleration(CALIBRATE_MOTOR_SPEED);
+  // stepper1.move(initialHoming);
+  // initialHoming -= 50;
+  stepper1.run();
+  delay(5);
+}
+
+void calibrateHome(){
+  while (digitalRead(HORZ_HOME_SW)){
+    stepper1.setMaxSpeed(CALIBRATE_MOTOR_SPEED);
+    stepper1.setSpeed(CALIBRATE_MOTOR_SPEED);
+    stepper1.move(initialHoming);
+    initialHoming -= 50;
+    stepper1.run();
+    delay(5);
+  }
+
+  stepper1.setCurrentPosition(0);
+  stepper1.setMaxSpeed(CALIBRATE_MOTOR_SPEED);
+  // stepper1.setAcceleration(CALIBRATE_MOTOR_SPEED);
+  initialHoming = 1;
   
+  while (!digitalRead(HORZ_HOME_SW)){
+    stepper1.moveTo(initialHoming);
+    initialHoming++;
+    stepper1.run();
+    delay(5);
+  }
+  stepper1.setCurrentPosition(0);
+  
+  // initialHoming = -1;
+  // while (digitalRead(VERT_HOME_SW)){
+  //   stepper2.setSpeed(CALIBRATE_MOTOR_SPEED);
+  //   stepper2.moveTo(initialHoming);
+  //   initialHoming--;
+  //   stepper2.run();
+  //   delay(5);
+  // }
+
+  // stepper2.setCurrentPosition(0);
+  // stepper2.setSpeed(CALIBRATE_MOTOR_SPEED);
+  // stepper2.setAcceleration(CALIBRATE_MOTOR_SPEED);
+  // initialHoming = 1;
+  
+  // while (!digitalRead(VERT_HOME_SW)){
+  //   stepper2.moveTo(initialHoming);
+  //   initialHoming++;
+  //   stepper2.run();
+  //   delay(5);
+  // }
+  // stepper2.setCurrentPosition(0);
 }
 
 String parseDataStream(){
--- simpleMotorTest/simpleMotorTest.ino
+++ simpleMotorTest/simpleMotorTest.ino
@@ -1,24 +1,51 @@
-int x;
+#define pulPinMotor 8
+#define dirPinMotor 7
+// #define pulPinMotor2 13
+// #define dirPinMotor2 4
+#define stepsPerRev 800
+#define stepperSpeed 700
+#define FOR 1
+#define BAC 0
+
+void stepperRun(int steps, int direction);
+
 void setup() {
-  pinMode(9, OUTPUT);  // set Pin9 as PUL
-  pinMode(8, OUTPUT);  // set Pin8 as DIR
+  pinMode(pulPinMotor, OUTPUT);  // set Pin9 as PUL
+  pinMode(dirPinMotor, OUTPUT);  // set Pin8 as DIR
 }
 void loop() {
-  digitalWrite(8, HIGH);     // set high level direction
-  for (x = 0; x < 400; x++)  // repeat 400 times a revolution when setting 400 on driver
+  // stepperRun(stepsPerRev, FOR);
+  // stepperRun(stepsPerRev, BAC, pulPinMotor1, dirPinMotor1);
+  stepperRun(stepsPerRev, FOR);
+  // stepperRun(stepsPerRev, BAC, pulPinMotor2, dirPinMotor2);
+  // stepperRun(stepsPerRev, FOR, pulPinMotor2, dirPinMotor2);
+  // digitalWrite(dirPin, test);     // set high level direction
+  // for (x = 0; x < stepsPerRev; x++)  // repeat 400 times a revolution when setting 400 on driver
+  // {
+  //   digitalWrite(pulPin, HIGH);   // Output high
+  //   delayMicroseconds(stepperSpeed);  // set rotate speed
+  //   digitalWrite(pulPin, LOW);    // Output low
+  //   delayMicroseconds(stepperSpeed);  // set rotate speed
+  // }
+  // delay(1000);           //pause 1 second
+  // digitalWrite(dirPin, BAC);  // set high level direction
+  // for (x = 0; x < stepsPerRev; x++) {
+  //   digitalWrite(pulPin, HIGH);
+  //   delayMicroseconds(stepperSpeed);
+  //   digitalWrite(pulPin, LOW);
+  //   delayMicroseconds(stepperSpeed);
+  // }
+  // delay(1000);
+}
+
+void stepperRun(int steps, int direction){  
+  digitalWrite(dirPinMotor, direction);     // set high level direction
+  for (int x = 0; x < steps; x++)  // repeat 400 times a revolution when setting 400 on driver
   {
-    digitalWrite(9, HIGH);   // Output high
-    delayMicroseconds(500);  // set rotate speed
-    digitalWrite(9, LOW);    // Output low
-    delayMicroseconds(500);  // set rotate speed
+    digitalWrite(pulPinMotor, HIGH);   // Output high
+    delayMicroseconds(stepperSpeed);  // set rotate speed
+    digitalWrite(pulPinMotor, LOW);    // Output low
+    delayMicroseconds(stepperSpeed);  // set rotate speed
   }
-  delay(1000);           //pause 1 second
-  digitalWrite(8, LOW);  // set high level direction
-  for (x = 0; x < 400; x++) {
-    digitalWrite(9, HIGH);
-    delayMicroseconds(500);
-    digitalWrite(9, LOW);
-    delayMicroseconds(500);
-  }
-  delay(1000);
+  delay(1000); // May be unnecessary
 }
