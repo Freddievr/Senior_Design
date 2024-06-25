86132e1af20748fb43a837da728040b8f8c272e5
Freddie Vasquez-Rios <161064448+Freddievr@users.noreply.github.com>
2024-06-25 10:56:46 -0700

    Add files via upload

--- freddieMotorTest/freddieMotorTest.ino
+++ freddieMotorTest/freddieMotorTest.ino
@@ -2,7 +2,7 @@
 #define dirPinMotor1 7
 #define pulPinMotor2 13
 #define dirPinMotor2 4
-#define stepsPerRev 800
+//#define stepsPerRev 800
 #define stepperSpeed 500
 #define FOR 1
 #define BAC 0
@@ -10,6 +10,7 @@ float Gap_Width;
 int Num_Fingers;
 int Curr_Finger;
 char myData[30] = {0};
+float stepsPerRev;
 
 void stepperRun(int steps, int direction, int pulPin, int dirPin);
 
@@ -30,17 +31,18 @@ void loop() {
     myData[m] = '\0'; //null-byte
  
     Gap_Width = atof(strtok(myData, ","));
+    stepsPerRev = (Gap_Width * 800);
     //Serial.print("Gap_Width = "); Serial.println(Gap_Width, 2);
 
     Num_Fingers = atoi(strtok(NULL, ","));
     //Serial.print("Num_Fingers = "); Serial.println(Num_Fingers);
     delay(1000);
     Serial.end();     }
-      
-  for(Curr_Finger = 0; Curr_Finger < Num_Fingers; Curr_Finger++){   
+    //ResetHoriz(1000);
+  for(Curr_Finger = 0; Curr_Finger < Num_Fingers; Curr_Finger++){     
       delay(500);
+
       stepperRun(stepsPerRev, BAC, pulPinMotor1, dirPinMotor1);
-      stepperRun(stepsPerRev, FOR, pulPinMotor1, dirPinMotor1);
       stepperRun(stepsPerRev/2, BAC, pulPinMotor2, dirPinMotor2);
       stepperRun(stepsPerRev/2, FOR, pulPinMotor2, dirPinMotor2);
       //Serial.print("Finger: #"); Serial.println(Curr_Finger+1);
@@ -49,6 +51,9 @@ void loop() {
         //Serial.print("Measurements Done");
         delay(1000);
         exit(0);
+
+
+        
       }
   }
 }
@@ -64,3 +69,17 @@ void stepperRun(int steps, int direction, int pulPin, int dirPin){
   }
   delay(1000);                      // May be unnecessary
 }
+
+void ResetHoriz(int steps){  
+    
+  digitalWrite(dirPinMotor1, FOR);  // set direction level
+  for (int x = 0; x < steps; x++)   // repeat "steps" times a revolution
+  {
+    digitalWrite(pulPinMotor1, HIGH);     // Output high
+    delayMicroseconds(stepperSpeed);// set rotate speed
+    digitalWrite(pulPinMotor1, LOW);      // Output low
+    delayMicroseconds(stepperSpeed);// set rotate speed
+  }
+  delay(1000);                      // May be unnecessary
+}
+
